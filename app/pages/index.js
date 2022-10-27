0d97850c383cfb4643afb9ae526f17f1f6bc98bf
putukus <putu@codekiwi.com>
2022-10-27 17:53:21 +0800

    add metadata for deploy

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,19 +1,54 @@
-import React from "react";
-
+import React, {useEffect} from "react";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 // Constants
 const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const Home = () => {
+    const wallet = useWallet();
+
+    // Actions
+    const renderNotConnectedContainer = () => (
+        <div>
+            <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
+
+            <div className="button-container">
+                <WalletMultiButton className="cta-button connect-wallet-button" />
+            </div>
+        </div>
+    );
+
+    
+  const checkIfWalletIsConnected = async () => {
+    // We're using optional chaining (question mark) to check if the object is null
+    if (window?.solana?.isPhantom) {
+        console.log("Phantom wallet found!");
+        console.log("Public key:", wallet.publicKey?.toString('hex'));
+    } else {
+        alert("Solana object not found! Get a Phantom Wallet 👻");
+    }
+  };
+
+    useEffect( () => {
+        const onLoad = async () => {
+        await checkIfWalletIsConnected();
+        };
+
+        onLoad();
+    }, []);
+    
     return (
         <div className="App">
             <div className="container">
                 <div className="header-container">
                     <p className="header">🍭 Candy Drop</p>
                     <p className="sub-text">NFT drop machine with fair mint</p>
+                     {/* Add the condition to show this only if we don't have a wallet address */}
+                     {wallet.publicKey ? "Hello World" : renderNotConnectedContainer()}
                 </div>
                 <div className="footer-container">
-                    <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+                    <img alt="Twitter Logo" className="twitter-logo" src="twitter-logo.svg" />
                     <a className="footer-text" href={TWITTER_LINK} target="_blank" rel="noreferrer">{`built on @${TWITTER_HANDLE}`}</a>
                 </div>
             </div>
